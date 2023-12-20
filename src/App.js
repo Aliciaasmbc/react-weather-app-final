e69a857cd49bf381ed388e3ccd3786b18f1d967c
Aliciaasmbc <alicia.apreala@gmail.com>
2023-12-20 14:46:03 +0000

    added github link

--- src/App.js
+++ src/App.js
@@ -5,6 +5,17 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello world </h1>
+      <p>
+        {" "}
+        This project was coded with 💜 by Alicia Apreala and is{" "}
+        <a
+          href="https://github.com/Aliciaasmbc/react-weather-app-final"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-sourced on Github
+        </a>
+      </p>
     </div>
   );
 }
