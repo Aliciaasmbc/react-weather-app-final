2a47080b8a7fbd34e67f969534bf1f5c4eee5a4e
Aliciaasmbc <alicia.apreala@gmail.com>
2023-12-20 15:47:57 +0000

    added bootstrap to project

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello world </h1>
-      <p>
-        {" "}
-        This project was coded with 💜 by Alicia Apreala and is{" "}
-        <a
-          href="https://github.com/Aliciaasmbc/react-weather-app-final"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open-sourced on Github
-        </a>
-      </p>
+      <div className="container">
+        <h1>Hello world </h1>
+
+        <p>
+          {" "}
+          This project was coded with 💜 by Alicia Apreala and is{" "}
+          <a
+            href="https://github.com/Aliciaasmbc/react-weather-app-final"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-sourced on Github
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
