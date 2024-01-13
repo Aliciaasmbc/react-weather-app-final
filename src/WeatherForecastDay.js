68d568bd4e1fdb5e196d9623597efde9875b2c5b
Aliciaasmbc <alicia.apreala@gmail.com>
2024-01-13 15:07:59 +0000

    de-bug forecast refresh, deleted farheneit and added style

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -22,15 +22,16 @@ export default function WeatherForecastDay(props) {
   }
 
   return (
-    <div>
+    <div className="WeatherForecast-wrap">
       <div className="WeatherForecast-day">{day()}</div>
       <WeatherIcon code={props.data.weather[0].icon} size={36} />
       <div className="WeatherForecast-temperatures">
-        <span className="WeatherForecast-temperature-max">
-          {maxTemperature()}
-        </span>
         <span className="WeatherForecast-temperature-min">
           {minTemperature()}
+        </span>
+        {""} {""}
+        <span className="WeatherForecast-temperature-max">
+          {maxTemperature()}
         </span>
       </div>
     </div>
